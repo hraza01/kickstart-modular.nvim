cb3b7d05b1b6198606dd69340ace362fb2d01b49
hraza01 <razahasan24@gmail.com>
2024-01-12 15:39:48 -0500

    mods

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,22 @@
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
+-- base options
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+
+-- BG transparency
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 -- Set highlight on search
 vim.o.hlsearch = false
 
