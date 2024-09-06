896a36250317e8a94119d7f0f3816f4de46899e7
Hasan Raza <razahasan24@gmail.com>
2024-09-06 01:23:01 -0400

    added formatter

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,3 +1,35 @@
+local clients_lsp = function()
+    -- Check if 'conform' is available
+    local status, conform = pcall(require, 'conform')
+    if not status then
+        return 'Conform not installed'
+    end
+
+    local lsp_format = require 'conform.lsp_format'
+
+    -- Get formatters for the current buffer
+    local formatters = conform.list_formatters_for_buffer()
+    if formatters and #formatters > 0 then
+        local formatterNames = {}
+
+        for _, formatter in ipairs(formatters) do
+            table.insert(formatterNames, formatter)
+        end
+
+        return '󰷈 ' .. table.concat(formatterNames, ' ')
+    end
+
+    -- Check if there's an LSP formatter
+    local bufnr = vim.api.nvim_get_current_buf()
+    local lsp_clients = lsp_format.get_format_clients { bufnr = bufnr }
+
+    if not vim.tbl_isempty(lsp_clients) then
+        return '󰷈 LSP Formatter'
+    end
+
+    return ''
+end
+
 return {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicon' },
@@ -21,7 +53,7 @@ return {
                     },
                 },
                 lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_x = { clients_lsp, 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' },
             },
